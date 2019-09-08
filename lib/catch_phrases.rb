2af62f6493bfe2f6861f4c097b22942f4e65c7f0
steven nguyen <sqnguyen829@gmail.com>
2019-09-08 20:31:19 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,3 +1,6 @@
+=begin
+
+#the givin code to start with 
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
@@ -5,4 +8,28 @@ end
 
 def toadstool
   puts status
+end
+
+=end
+
+
+#Starting from scratch
+
+def mario
+  phrase = "It's-a me, Mario!"
+  puts phrase
+end
+
+def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+end
+
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def any_phrase(phrase)
+  puts phrase
 end
